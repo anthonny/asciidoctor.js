947e8f52f8d1b17567d7107f43da7c147119d1e5
Anthonny <anthonny.querouil@gmail.com>
2016-03-30 10:07:46 +0200

    fix(test): fix indent

--- spec/share/common-specs.js
+++ spec/share/common-specs.js
@@ -92,60 +92,60 @@ var commonSpec = function(platform, Opal, Asciidoctor) {
         var options = Opal.hash({'attributes': ['backend=docbook45', 'doctype=book'],'header_footer':true});
         var html = Asciidoctor.$convert(':doctitle: DocTitle\n:docdate: 2014-01-01\n== Test', options);
         expect(html).toContain('<?xml version="1.0" encoding="UTF-8"?>\n\
-        <!DOCTYPE book PUBLIC "-//OASIS//DTD DocBook XML V4.5//EN" "http://www.oasis-open.org/docbook/xml/4.5/docbookx.dtd">\n\
-        <?asciidoc-toc?>\n\
-        <?asciidoc-numbered?>\n\
-        <book lang="en">\n\
-          <bookinfo>\n\
-            <title>DocTitle</title>\n\
-              <date>2014-01-01</date>\n\
-              </bookinfo>\n\
-              <chapter id="_test">\n\
-                <title>Test</title>\n\
-                  \n\
-                </chapter>\n\
-              </book>');
+<!DOCTYPE book PUBLIC "-//OASIS//DTD DocBook XML V4.5//EN" "http://www.oasis-open.org/docbook/xml/4.5/docbookx.dtd">\n\
+<?asciidoc-toc?>\n\
+<?asciidoc-numbered?>\n\
+<book lang="en">\n\
+<bookinfo>\n\
+<title>DocTitle</title>\n\
+<date>2014-01-01</date>\n\
+</bookinfo>\n\
+<chapter id="_test">\n\
+<title>Test</title>\n\
+\n\
+</chapter>\n\
+</book>');
             });
 
-            it('backend=docbook5 should produce a docbook5 document', function() {
-              var options = Opal.hash({'attributes': ['backend=docbook5', 'doctype=book'],'header_footer':true});
-              var html = Asciidoctor.$convert(':doctitle: DocTitle\n:docdate: 2014-01-01\n== Test', options);
-              expect(html).toContain('<?xml version="1.0" encoding="UTF-8"?>\n\
-              <?asciidoc-toc?>\n\
-              <?asciidoc-numbered?>\n\
-              <book xmlns="http://docbook.org/ns/docbook" xmlns:xl="http://www.w3.org/1999/xlink" version="5.0" xml:lang="en">\n\
-                <info>\n\
-                  <title>DocTitle</title>\n\
-                    <date>2014-01-01</date>\n\
-                    </info>\n\
-                    <chapter xml:id="_test">\n\
-                      <title>Test</title>\n\
-                        \n\
-                      </chapter>\n\
-                    </book>');
-                  });
+      it('backend=docbook5 should produce a docbook5 document', function() {
+        var options = Opal.hash({'attributes': ['backend=docbook5', 'doctype=book'],'header_footer':true});
+        var html = Asciidoctor.$convert(':doctitle: DocTitle\n:docdate: 2014-01-01\n== Test', options);
+        expect(html).toContain('<?xml version="1.0" encoding="UTF-8"?>\n\
+<?asciidoc-toc?>\n\
+<?asciidoc-numbered?>\n\
+<book xmlns="http://docbook.org/ns/docbook" xmlns:xl="http://www.w3.org/1999/xlink" version="5.0" xml:lang="en">\n\
+<info>\n\
+<title>DocTitle</title>\n\
+<date>2014-01-01</date>\n\
+</info>\n\
+<chapter xml:id="_test">\n\
+<title>Test</title>\n\
+\n\
+</chapter>\n\
+</book>');
+        });
 
-                  it('should produce a html5 document with font icons', function() {
-                    var options = Opal.hash({'attributes': 'icons=font@'});
-                    var html = Asciidoctor.$convert('= Document\n\nThis is a simple document.\n\n== Section\n\nCAUTION: This is important!', options);
-                    expect(html).toContain('<i class="fa icon-caution" title="Caution"></i>');
-                  });
+      it('should produce a html5 document with font icons', function() {
+        var options = Opal.hash({'attributes': 'icons=font@'});
+        var html = Asciidoctor.$convert('= Document\n\nThis is a simple document.\n\n== Section\n\nCAUTION: This is important!', options);
+        expect(html).toContain('<i class="fa icon-caution" title="Caution"></i>');
+      });
 
-                });
+    });
 
-                describe('Include', function() {
-                  it('Should include file', function() {
-                    var opts = Opal.hash({'safe': 'safe'});
-                    var html = Asciidoctor.$convert('include::spec/share/include.adoc[]', opts);
-                    expect(html).toContain('include content');
-                  });
+    describe('Include', function() {
+      it('Should include file', function() {
+        var opts = Opal.hash({'safe': 'safe'});
+        var html = Asciidoctor.$convert('include::spec/share/include.adoc[]', opts);
+        expect(html).toContain('include content');
+      });
 
-                  it('Should include csv file in table', function() {
-                    var opts = Opal.hash({'safe': 'safe'});
-                    var html = Asciidoctor.$convert(',===\ninclude::spec/share/sample.csv[]\n,===', opts);
-                    expect(html).toContain('March');
-                  });
-                });
+      it('Should include csv file in table', function() {
+        var opts = Opal.hash({'safe': 'safe'});
+        var html = Asciidoctor.$convert(',===\ninclude::spec/share/sample.csv[]\n,===', opts);
+        expect(html).toContain('March');
+      });
+    });
   });
 
 }
