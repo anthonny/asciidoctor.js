a007867a26d0552a71302de1fead8dfe08f1b7c3
Anthonny <anthonny.querouil@gmail.com>
2016-03-30 09:57:23 +0200

    fix(test): add XMLHttpRequest in bower tests

--- spec/bower/bower.spec.js
+++ spec/bower/bower.spec.js
@@ -1,1 +1,5 @@
+var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+// Define overrideMimeType, not define by default in wrapper
+XMLHttpRequest.prototype.overrideMimeType = function() {};
+
 commonSpec('Bower', Opal, Opal.Asciidoctor);
